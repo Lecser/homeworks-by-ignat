369ef57414958ef3493f383ae9460a49ee75e57d
Lecser <Lecser025@gmail.com>
2022-09-19 07:10:55 +0300

    add btn

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { PATH } from "./Pages";
+import classes from "./Header.module.css";
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
-
+  return (
+    <div>
+      <div className={classes.dropdown}>
+        <button className={classes.dropBtn}>Dropdown</button>
+        <div className={classes.dropdownСontent}>
+          <Link to={PATH.PRE_JUNIOR}> Pre-junior </Link>
+          <Link to={PATH.JUNIOR}> junior </Link>
+          <Link to={PATH.JUNIOR_PLUS}> junior+ </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
