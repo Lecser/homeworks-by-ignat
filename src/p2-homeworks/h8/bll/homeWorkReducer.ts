b56778339270554874ef2adaa8c090601677d9be
Lecser <Lecser025@gmail.com>
2022-10-12 00:38:26 +0300

    done hw8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,42 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+import { UserType } from "../HW8";
+
+type ActionType = checkAgeACType | sortACType;
+export const homeWorkReducer = (
+  state: UserType,
+  action: ActionType
+): UserType => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      let sorted = [...state].sort((a, b) =>
+        a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+      );
+      return action.payload.value === "up" ? sorted : sorted.reverse();
     }
-}
+    case "check": {
+      // need to fix
+      return state.filter((el) => el.age > action.payload.age);
+    }
+    default:
+      return state;
+  }
+};
+type sortACType = ReturnType<typeof sortAC>;
+type checkAgeACType = ReturnType<typeof checkAgeAC>;
+export const sortAC = (value: string) => {
+  return {
+    type: "sort",
+    payload: {
+      value,
+    },
+  } as const;
+};
+
+export const checkAgeAC = (age: number) => {
+  return {
+    type: "check",
+    payload: {
+      age,
+    },
+  } as const;
+};
